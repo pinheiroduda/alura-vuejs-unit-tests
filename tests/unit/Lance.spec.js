26dbe22b314cdc299186dc325014e9994cb78f9f
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-13 15:06:09 -0300

    ✨ add: test if emitted money toss is equal to input value

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -21,3 +21,13 @@ test("emite um lance quando o valor é maior do que zero", () => {
   const lancesEmitidos = wrapper.emitted("novo-lance");
   expect(lancesEmitidos).toHaveLength(1);
 });
+
+test("emite o valor esperado de um lance válido", () => {
+  const wrapper = mount(Lance);
+  const input = wrapper.find("input");
+  input.setValue(100);
+  wrapper.trigger("submit");
+  const lancesEmitidos = wrapper.emitted("novo-lance");
+  const lance = parseInt(lancesEmitidos[0][0]);
+  expect(lance).toBe(100);
+});
