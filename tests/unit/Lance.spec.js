f6989268a6e907e03eee32acd012c2d148a25a86
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-13 15:02:09 -0300

    ✅ add: test if value is emmited when it has a valid value

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -12,3 +12,12 @@ test("não aceita lance com valor menor do que zero", () => {
   wrapper.trigger("submit");
   expect(lancesEmitidos).toBeUndefined();
 });
+
+test("emite um lance quando o valor é maior do que zero", () => {
+  const wrapper = mount(Lance);
+  const input = wrapper.find("input");
+  input.setValue(100);
+  wrapper.trigger("submit");
+  const lancesEmitidos = wrapper.emitted("novo-lance");
+  expect(lancesEmitidos).toHaveLength(1);
+});
