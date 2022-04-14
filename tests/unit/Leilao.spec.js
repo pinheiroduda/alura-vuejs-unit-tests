cae6a61da431634122328be5a3994e7cf2dd300c
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-14 10:07:26 -0300

    ✅ add: tests whether auction data is displayed on the card

---
+++ tests/unit/Leilao.spec.js
@@ -0,0 +1,32 @@
+/* eslint-disable quotes */
+/* eslint-disable semi */
+
+import Leilao from "@/components/Leilao";
+import { mount } from "@vue/test-utils";
+
+const leilao = {
+  produto: "Um livro da casa do código",
+  lanceInicial: 49,
+  descricao: "Um maravilhoso livro sobre Vue"
+};
+
+describe("Um leilão exibe os dados do produto", () => {
+  test("exibe os dados do leilão no card", () => {
+    const wrapper = mount(Leilao, {
+      propsData: {
+        leilao
+      }
+    });
+    const header = wrapper.find(".card-header").element;
+    const title = wrapper.find(".card-title").element;
+    const text = wrapper.find(".card-text").element;
+
+    expect(header.textContent).toContain(
+      `Estamos leiloando um(a): ${leilao.produto}`
+    );
+    expect(title.textContent).toContain(
+      `Lance inicial: R$ ${leilao.lanceInicial}`
+    );
+    expect(text.textContent).toContain(leilao.descricao);
+  });
+});
