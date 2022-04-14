e2a6d734517b27981150b0dc7c66a50443abc161
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-14 09:20:46 -0300

    ✅ add: teste if a expected money toss valid value is emitted

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -47,4 +47,18 @@ describe("Um lance com valor mínimo", () => {
     const lancesEmitidos = wrapper.emitted("novo-lance");
     expect(lancesEmitidos).toHaveLength(1);
   });
+
+  test("emite o valor esperado de um lance válido", () => {
+    const wrapper = mount(Lance, {
+      propsData: {
+        lanceMinimo: 300
+      }
+    });
+    const input = wrapper.find("input");
+    input.setValue(400);
+    wrapper.trigger("submit");
+    const lancesEmitidos = wrapper.emitted("novo-lance");
+    const valorDoLance = parseInt(lancesEmitidos[0][0]);
+    expect(valorDoLance).toBe(400);
+  });
 });
