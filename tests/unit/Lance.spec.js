3f5a61893b441e6709e12a4144c5cd92b931debc
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-14 09:10:17 -0300

    ✨ feat: group tests using describre

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -4,30 +4,32 @@
 import Lance from "@/components/Lance";
 import { mount } from "@vue/test-utils";
 
-test("não aceita lance com valor menor do que zero", () => {
-  const wrapper = mount(Lance);
-  const input = wrapper.find("input");
-  input.setValue(-100);
-  const lancesEmitidos = wrapper.emitted("novo-lance");
-  wrapper.trigger("submit");
-  expect(lancesEmitidos).toBeUndefined();
-});
+describe("Um lance sem valor mínimo", () => {
+  test("não aceita lance com valor menor do que zero", () => {
+    const wrapper = mount(Lance);
+    const input = wrapper.find("input");
+    input.setValue(-100);
+    const lancesEmitidos = wrapper.emitted("novo-lance");
+    wrapper.trigger("submit");
+    expect(lancesEmitidos).toBeUndefined();
+  });
 
-test("emite um lance quando o valor é maior do que zero", () => {
-  const wrapper = mount(Lance);
-  const input = wrapper.find("input");
-  input.setValue(100);
-  wrapper.trigger("submit");
-  const lancesEmitidos = wrapper.emitted("novo-lance");
-  expect(lancesEmitidos).toHaveLength(1);
-});
+  test("emite um lance quando o valor é maior do que zero", () => {
+    const wrapper = mount(Lance);
+    const input = wrapper.find("input");
+    input.setValue(100);
+    wrapper.trigger("submit");
+    const lancesEmitidos = wrapper.emitted("novo-lance");
+    expect(lancesEmitidos).toHaveLength(1);
+  });
 
-test("emite o valor esperado de um lance válido", () => {
-  const wrapper = mount(Lance);
-  const input = wrapper.find("input");
-  input.setValue(100);
-  wrapper.trigger("submit");
-  const lancesEmitidos = wrapper.emitted("novo-lance");
-  const lance = parseInt(lancesEmitidos[0][0]);
-  expect(lance).toBe(100);
+  test("emite o valor esperado de um lance válido", () => {
+    const wrapper = mount(Lance);
+    const input = wrapper.find("input");
+    input.setValue(100);
+    wrapper.trigger("submit");
+    const lancesEmitidos = wrapper.emitted("novo-lance");
+    const lance = parseInt(lancesEmitidos[0][0]);
+    expect(lance).toBe(100);
+  });
 });
