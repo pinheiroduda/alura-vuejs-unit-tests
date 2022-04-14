793da911d6d48655fffa84665f65c9a07bda20a9
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-14 09:16:04 -0300

    ✅ add: teste if money toss have a value bigger than the minimum reported

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -33,3 +33,18 @@ describe("Um lance sem valor mínimo", () => {
     expect(lance).toBe(100);
   });
 });
+
+describe("Um lance com valor mínimo", () => {
+  test("todos os lances devem possuir um valor maior do que o mínimo informado", () => {
+    const wrapper = mount(Lance, {
+      propsData: {
+        lanceMinimo: 300
+      }
+    });
+    const input = wrapper.find("input");
+    input.setValue(400);
+    wrapper.trigger("submit");
+    const lancesEmitidos = wrapper.emitted("novo-lance");
+    expect(lancesEmitidos).toHaveLength(1);
+  });
+});
