b3c57511c17169165fd1ff7f14f4352f0f25e142
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-14 11:04:34 -0300

    ✅ add: test if when form is filled a new auction is created

---
+++ tests/unit/NovoLeilao.js
@@ -0,0 +1,32 @@
+/* eslint-disable quotes */
+/* eslint-disable semi */
+/* eslint-disable no-undef */
+
+import { createLeilao } from "@/http";
+import NovoLeilao from "@/views/NovoLeilao";
+import { mount } from "@vue/test-utils";
+import flushPromises from "flush-promises";
+
+jest.mock("@/http");
+
+const $router = {
+  push: jest.fn()
+};
+
+describe("Um novo leilão deve ser criado", () => {
+  test("dado o formulário preenchido, um leilão deve ser criado", () => {
+    createLeilao.mockResolvedValueOnce();
+
+    const wrapper = mount(NovoLeilao, {
+      mocks: {
+        $router
+      }
+    });
+    wrapper.find(".produto").setValue("Um livro da casa do código");
+    wrapper.find(".descricao").setValue("Conteúdo de primeira");
+    wrapper.find(".valor").setValue(50);
+    wrapper.find("form").trigger("submit");
+
+    expect(createLeilao).toHaveBeenCalled();
+  });
+});
